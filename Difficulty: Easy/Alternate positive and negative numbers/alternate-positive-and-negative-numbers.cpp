64db1efcf5be7d0ee77282b3aa1756ca089f0b9f
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-09-14 22:00:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Alternate positive and negative numbers/alternate-positive-and-negative-numbers.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    void rearrange(vector<int> &arr) {
+        // code here
+        vector<int> pos, neg; int a = 0, b = 0;
+        for ( auto i : arr ) (i >= 0) ? pos.push_back(i) : neg.push_back(i);
+        for ( int i = 0; i<arr.size(); i++ ){
+            if ( i%2 == 0 ){ 
+                if ( a < pos.size() ) arr[i] = pos[a++];
+                else arr[i] = neg[b++];
+            } else {
+                if ( b < neg.size() ) arr[i] = neg[b++];
+                else arr[i] = pos[a++];
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        int num;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        ob.rearrange(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
