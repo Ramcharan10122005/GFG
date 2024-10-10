df383f29c88d89cb9e33c6761750e9b24253ca6e
Ramcharan Kundena <160163532+Ramcharan10122005@users.noreply.github.com>
2024-10-11 00:18:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reorganize The Array/reorganize-the-array.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> rearrange(const vector<int>& arr) {
+        // Code here
+         unordered_map<int,int>map;
+        for(auto x:arr){
+            map[x]++;
+        }
+        vector<int>ans;
+        
+        for(int i=0;i<arr.size();i++){
+            if(map.find(i)==map.end()){
+                ans.push_back(-1);
+            }else{
+                 ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        vector<int> arr;
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution solution;
+        vector<int> ans = solution.rearrange(arr);
+
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+// } Driver Code Ends
